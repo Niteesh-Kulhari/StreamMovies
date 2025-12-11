3a585c666f29c7871b61241046e9b306826f3363
Niteesh kulhari <niteeshkulhari@mac.mynetworksettings.com>
2025-12-10 20:11:09 -0500

    Basic routes

--- Server/StreamMoviesServer/main.go
+++ Server/StreamMoviesServer/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	controller "github.com/Niteesh-Kulhari/StreamMovies/Server/StreamMovieServer/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +15,8 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello from Server")
 	})
+
+	router.GET("/movies", controller.GetMovies())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
