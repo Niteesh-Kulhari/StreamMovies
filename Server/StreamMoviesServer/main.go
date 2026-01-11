1589f73dd810b712c1f348ddb852418663bff13d
Niteesh Kulhari <nkulhari96@gmail.com>
2026-01-10 23:18:19 -0500

    fixes

--- Server/StreamMoviesServer/main.go
+++ Server/StreamMoviesServer/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 
+<<<<<<< HEAD
 	"github.com/Niteesh-Kulhari/StreamMovies/Server/StreamMovieServer/routes"
+=======
+	controller "github.com/Niteesh-Kulhari/StreamMovies/Server/StreamMovieServer/controllers"
+
+>>>>>>> 8de748d (Basic routes)
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,6 +23,8 @@ func main() {
 
 	routes.SetupUnProtectedRoutes(router)
 	routes.SetupProtectedRoutes(router)
+
+	router.GET("/movies", controller.GetMovies())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
